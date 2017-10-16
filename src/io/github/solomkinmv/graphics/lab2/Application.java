d479c471571723d11e8110171266b66c50ff889e
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-17 00:57:01 +0300

    Remove redundant lines

--- src/io/github/solomkinmv/graphics/lab2/Application.java
+++ src/io/github/solomkinmv/graphics/lab2/Application.java
@@ -4,8 +4,6 @@ import io.github.solomkinmv.graphics.lab2.figures.Cylinder;
 import io.github.solomkinmv.graphics.lab2.interactive.ExitOnClose;
 
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class Application {
     private static final int SIZE = 1000;
@@ -20,11 +18,5 @@ public class Application {
         fr.setSize(SIZE, SIZE);
         fr.add(new GraphicCanvas(Cylinder::new, SIZE, SIZE));
         fr.setVisible(true);
-        fr.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                fr.dispose();
-            }
-        });
-
     }
 }
