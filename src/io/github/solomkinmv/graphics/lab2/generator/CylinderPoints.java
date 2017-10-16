d8b4eb27cd2480b519d2fb75954754a576d61e34
Maksym Solomkin <solomkinmv@gmail.com>
2017-10-16 23:15:04 +0300

    Implement generator for the cylinder points

---
+++ src/io/github/solomkinmv/graphics/lab2/generator/CylinderPoints.java
@@ -0,0 +1,51 @@
+package io.github.solomkinmv.graphics.lab2.generator;
+
+import io.github.solomkinmv.graphics.lab2.points.Point3D;
+
+import java.util.stream.DoubleStream;
+
+public class CylinderPoints {
+
+    private final int r;
+    private final int h;
+    private final int hParts;
+    private final int vParts;
+
+    public CylinderPoints(int r, int h, int hParts, int vParts) {
+        this.r = r;
+        this.h = h;
+        this.hParts = hParts;
+        this.vParts = vParts;
+    }
+
+    public CylinderPoints(int r, int h) {
+        this.r = r;
+        this.h = h;
+        vParts = 10;
+        hParts = 100;
+    }
+
+    public Point3D[][] generatePoints() {
+        double step = 1. / vParts;
+
+        return DoubleStream.iterate(0, (v) -> v <= 1, (v) -> v + step)
+                           .mapToObj(this::generatePointsLayer)
+                           .toArray(Point3D[][]::new);
+    }
+
+    private double getRad(double u) {
+        return 2 * Math.PI * u;
+    }
+
+    private Point3D[] generatePointsLayer(double v) {
+        double step = 1. / hParts;
+
+        return DoubleStream.iterate(0, (u) -> u <= 1, (u) -> u + step)
+                           .mapToObj(u -> getPoint(u, v))
+                           .toArray(Point3D[]::new);
+    }
+
+    private Point3D getPoint(double u, double v) {
+        return new Point3D(r * Math.cos(getRad(u)), r * Math.sin(getRad(u)), h * v);
+    }
+}
